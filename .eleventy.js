14f5883d743f38f8c7c3e1fa534003f76f30f1a6
Philip Borenstein <pborenstein@gmail.com>
2019-08-04 21:03:38 -0400

    Add configuration settings

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,22 @@ module.exports = function(eleventyConfig) {
 
   return {
     dir: {
-      input: "src",
-      output: "_site"
-    }
+      input:    "src",
+      output:   "dist",
+      
+      //  These are inside the `input` directory
+      includes: "_includes",
+      data:     "_data"
+    },
+    templateFormats: [
+      "md",
+      "njk",
+      "html"
+    ],
+    dataTemplateEngine:     "njk",
+    htmlTemplateEngine:     "njk",
+    markdownTemplateEngine: "njk",
+    passthroughFileCopy:     true,
+    pathPrefix:              "/"
   }
 }
