29257059c48f07d7f75b933481f0f8e74ba81e8b
Philip Borenstein <pborenstein@gmail.com>
2019-09-15 20:59:15 -0400

    fix footer to be prettier

--- src/filters/has-mermaid.js
+++ src/filters/has-mermaid.js
@@ -1,5 +1,5 @@
 module.exports = function hasMermaid(content, opt) {  
-  if (content.indexOf('class="mermaid"') !== -1) {
+  if (content.includes('class="mermaid"')) {
     return `<script src="https://unpkg.com/mermaid/dist/mermaid.min.js"></script>
   <script>mermaid.initialize({ startOnLoad: true });</script>
 `;
--- src/pages/about.md
+++ src/pages/about.md
@@ -1,6 +1,9 @@
 ---
-date: 2018-01-01
+date: 1929-01-01
 title: About
+layout: layouts/page.njk
+tags:
+  - _nav
 ---
 
 
