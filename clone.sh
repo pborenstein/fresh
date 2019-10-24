1a7b436d0a99e5d1db5fc8ad5afe7f84fa04eab8
Philip Borenstein <pborenstein@gmail.com>
2019-10-24 18:28:40 -0400

    cloning shenanigans

--- clone.sh
+++ clone.sh
@@ -28,3 +28,23 @@ hub create pb.frish
 
 
 git push -u origin master
+
+######################
+
+change tempplate
+
+[master =]$ git fetch source
+remote: Enumerating objects: 5, done.
+remote: Counting objects: 100% (5/5), done.
+remote: Compressing objects: 100% (3/3), done.
+remote: Total 3 (delta 2), reused 0 (delta 0), pack-reused 0
+Unpacking objects: 100% (3/3), done.
+From github.com:pborenstein/fresh
+   8f2b664..d13b9c5  master     -> source/master
+   
+philip@ningal: ~/projects/pb.frish
+[master =]$ git merge source/master --allow-unrelated-histories
+CONFLICT (add/add): Merge conflict in README.md
+Auto-merging README.md
+Recorded preimage for 'README.md'
+Automatic merge failed; fix conflicts and then commit the result.
